76e0bc7f77d75b9a1dc930d2f62a2b65ef60f122
Pratham Gadkari <prathamgadkari@gmail.com>
2025-11-16 21:54:14 +0530

    github webhook

--- backend/handlers/github.go
+++ backend/handlers/github.go
@@ -1,9 +1,34 @@
 package handlers
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"pickel-backend/utils"
+)
+
 type GithubPushPayload struct {
 	Ref        string `json:"ref"`
 	Repository struct {
 		CloneURL string `json:"clone_url"`
 	} `json:"repository"`
 	After string `json:"after"`
+}
+
+func GithubWebhook(w http.ResponseWriter, r *http.Request) {
+	var payload GithubPushPayload
+	json.NewDecoder(r.Body).Decode(&payload)
+
+	if filepath.Base(payload.Ref) != "main" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	modelID := "sample"
+	go func() {
+		if err := utils.TriggerDeploy(payload.Repository.CloneURL, payload.After, modelID); err != nil {
+			fmt.Println("Deployment failed:", err)
+		}
+	}()
 }
