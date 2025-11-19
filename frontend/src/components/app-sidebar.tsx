08bfcc223a501a0bbc0fe22133d909a9015bfa3f
Pratham Gadkari <prathamgadkari@gmail.com>
2025-11-19 23:49:08 +0530

    routing change:

--- frontend/src/components/app-sidebar.tsx
+++ frontend/src/components/app-sidebar.tsx
@@ -1,55 +1,59 @@
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarGroupContent,
-  SidebarGroupLabel,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { Card } from "./ui/card"
-import { Settings } from "lucide-react"
-import UserCard from "./UserCard"
+  import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+  } from "@/components/ui/sidebar"
+  import { Card } from "./ui/card"
+  import { Settings } from "lucide-react"
+  import UserCard from "./UserCard"
+  import { redirect } from "next/navigation"
+import Link from "next/link"
 
-export function AppSidebar() {
-  return (
-    <Sidebar>
-      <SidebarHeader >
-        <Card className="text-left text-2xl px-2 py-3 font-bold rounded-md">Pickel</Card>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarGroup >
-          <SidebarGroupLabel>Actions</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
+  export function AppSidebar() {
+    return (
+      <Sidebar>
+        <SidebarHeader >
+          <Card className="text-left text-2xl px-2 py-3 font-bold rounded-md">Pickel</Card>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup >
+            <SidebarGroupLabel>Actions</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
 
-              <SidebarMenuItem>
-                <SidebarMenuButton>Dashboard</SidebarMenuButton>
-              </SidebarMenuItem>
+                <SidebarMenuItem>
+                   <Link href="/dashboard">
+                  <SidebarMenuButton>Dashboard</SidebarMenuButton>
+                  </Link>
+                </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <SidebarMenuButton>Models</SidebarMenuButton>
-              </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Models</SidebarMenuButton>
+                </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <SidebarMenuButton>Deployments</SidebarMenuButton>
-              </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Deployments</SidebarMenuButton>
+                </SidebarMenuItem>
 
-              <SidebarMenuItem>
-                <SidebarMenuButton>Ingress</SidebarMenuButton>
-              </SidebarMenuItem>
+                <SidebarMenuItem>
+                  <SidebarMenuButton>Ingress</SidebarMenuButton>
+                </SidebarMenuItem>
 
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter >
-        <UserCard />
-      </SidebarFooter>
-    </Sidebar>
-  )
-}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          <SidebarGroup />
+        </SidebarContent>
+        <SidebarFooter >
+          <UserCard />
+        </SidebarFooter>
+      </Sidebar>
+    )
+  }
