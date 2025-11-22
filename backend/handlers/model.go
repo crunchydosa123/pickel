cb1ca7ae0e212f9aea397d483d7fa045112a7e59
Pratham Gadkari <prathamgadkari@gmail.com>
2025-11-22 23:24:52 +0530

    todo comment

--- backend/handlers/model.go
+++ backend/handlers/model.go
@@ -234,6 +234,7 @@ func UpdateInstallationID(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//TODO: modify
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(map[string]interface{}{
 		"success": true,
